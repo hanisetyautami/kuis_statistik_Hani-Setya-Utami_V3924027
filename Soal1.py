1a8c8b3cfb4f92b518ec9b807eb27e13286b262f
hanisetyautami <hanistyautmi29@student.uns.ac.id>
2025-06-25 04:41:48 +1200

    Soal1.py

--- Soal1.py
+++ Soal1.py
@@ -1,31 +1,19 @@
 from sklearn.linear_model import LinearRegression
 import numpy as np
-import matplotlib.pyplot as plt
 
 # Data
-x = np.array([10, 20, 30, 40, 50]).reshape(-1, 1)
-y = np.array([100000, 200000, 400000, 550000, 800000])
+X = np.array([10, 20, 30, 40, 50]).reshape(-1, 1)
+Y = np.array([200, 400, 600, 800, 1000])
 
 # Model
 model = LinearRegression()
-model.fit(x, y)
+model.fit(X, Y)
 
-# Prediksi untuk 25 pelanggan
-x_pred = np.array([[25]])
+# Prediksi untuk 35 pelanggan
+x_pred = np.array([[35]])
 y_pred = model.predict(x_pred)
 
 # Output
-print("Koefisien (a):", model.coef_[0])
-print("Intersep (b):", model.intercept_)
-print("Prediksi pengeluaran untuk 25 pelanggan:", y_pred[0])
-
-# Plot data dan garis regresi
-plt.scatter(x, y, color='blue', label='Data asli')
-plt.plot(x, model.predict(x), color='red', label='Regresi linear')
-plt.scatter(x_pred, y_pred, color='green', label='Prediksi (25 pelanggan)')
-plt.title('Regresi Linear: pelanggan vs pengeluaran')
-plt.xlabel('Jumlah pelanggan')
-plt.ylabel('Pengeluaran (ribu rupiah)')
-plt.legend()
-plt.grid(True)
-plt.show()
+print("Koefisien (b):", model.coef_[0])
+print("Intercept (a):", model.intercept_)
+print("Prediksi pendapatan untuk 35 pelanggan:", y_pred[0])
