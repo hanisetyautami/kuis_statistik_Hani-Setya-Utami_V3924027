77807e9a9e6640583a5b17afa01bee66409aa1d8
hanisetyautami <hanistyautmi29@student.uns.ac.id>
2025-06-25 04:46:33 +1200

    Update Soal3.py

--- Soal3.py
+++ Soal3.py
@@ -2,26 +2,31 @@ from sklearn.linear_model import LinearRegression
 import numpy as np
 import matplotlib.pyplot as plt
 
-# data
-x = np.array([1,2,3,4,5]).reshape(-1,1)
-y = np.array([20,40,50,65,80])
+# Data
+X = np.array([1, 3, 5, 7, 9]).reshape(-1, 1)
+Y = np.array([150, 120, 90, 60, 30])
 
+# Buat model regresi
 model = LinearRegression()
-model.fit(x,y)
+model.fit(X, Y)
 
-# prediksi nilai
-x_pred = np.array([[4]])
-prediksi = model.predict(x_pred)
+# Prediksi nilai Y
+Y_pred = model.predict(X)
 
-print(f"Persamaan regresi: Y = {round(model.intercept_,2)} + {round(model.coef_[0],2)}X")
-print(f"Prediksi harga jual jika ukuran = 6 adalah: {round(prediksi[0],2)} juta rupiah")
+# Prediksi harga untuk usia 4 tahun
+prediksi_4_tahun = model.predict([[4]])
 
-plt.scatter(x,y, color='blue', label='data asli')
-plt.plot(x, model.predict(x), color='red', label='Regresi linear')
-plt.scatter(x_pred, prediksi, color='green', label='Prediksi (4 Tahun)')
-plt.xlabel('Ukuran rumah')
-plt.ylabel('Harga jual (juta rupiah)')
-plt.title('Regresi Linear')
+# Output
+print("Persamaan regresi: Y = {:.2f} + {:.2f}X".format(model.intercept_, model.coef_[0]))
+print("Prediksi harga jual untuk usia 4 tahun:", prediksi_4_tahun[0], "juta rupiah")
+
+# Plot
+plt.scatter(X, Y, color='blue', label='Data Asli')
+plt.plot(X, Y_pred, color='red', label='Garis Regresi')
+plt.scatter(4, prediksi_4_tahun, color='green', label='Prediksi (4 tahun)')
+plt.title('Usia Kendaraan vs Harga Jual')
+plt.xlabel('Usia (tahun)')
+plt.ylabel('Harga Jual (juta rupiah)')
 plt.legend()
 plt.grid(True)
 plt.show()
